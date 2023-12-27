9f35b35a9f4ca844609328e0845b979b1b6b36d5
Abdullah <Abdullah.gb@hotmail.com>
2023-12-27 20:58:03 +0500

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorHotchocolate;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddRedApi().ConfigureHttpClient(client => client.BaseAddress = new Uri("https://api-red.metaphorltd.com/graphql"));
+
+await builder.Build().RunAsync();
