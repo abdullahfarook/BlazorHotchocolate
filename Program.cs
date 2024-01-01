5e149ed99aaaf0d621bcfcc18b0a9f784516a99c
Abdullah <abdullah@metaphorltd.com>
2024-01-01 07:05:04 +0500

    imp: infinite scroll

--- Program.cs
+++ Program.cs
@@ -1,12 +1,16 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using BlazorHotchocolate;
+using StrawberryShake;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddRedApi().ConfigureHttpClient(client => client.BaseAddress = new Uri("https://api-red.metaphorltd.com/graphql"));
+builder.Services.AddRedApi(ExecutionStrategy.CacheFirst).ConfigureHttpClient(client =>
+{
+    client.BaseAddress = new Uri("https://api-red.metaphorltd.com/graphql");
+});
 
 await builder.Build().RunAsync();
